3d4ec265b180d5db36ee74a4aa6d0d05840bbeed
GSerjo <sedot8@gmail.com>
2015-01-04 21:27:16 +0300

    + Client

--- Source/BookService/Program.cs
+++ Source/BookService/Program.cs
@@ -1,11 +1,30 @@
 ﻿using System;
+using StackExchange.Redis;
 
 namespace BookService
 {
     internal class Program
     {
-        private static void Main(string[] args)
+//        private ConnectionMultiplexer _redis = ConnectionMultiplexer.Connect("localhost");
+
+        private static void Main()
         {
+//            while (RunProgram())
+//            {
+//                Console.WriteLine("wer");
+//            }
+
+//            Console.ReadKey();
         }
+
+//        private static bool RunProgram()
+//        {
+//            ConsoleKeyInfo key = Console.ReadKey();
+//            if (key.Key == ConsoleKey.Escape)
+//            {
+//                return false;
+//            }
+//            return true;
+//        }
     }
 }
---
+++ Source/Client/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using StackExchange.Redis;
+
+namespace Client
+{
+    internal class Program
+    {
+        private static ConnectionMultiplexer _redis;
+
+        private static void GetData()
+        {
+            IDatabase database = _redis.GetDatabase();
+            RedisValue[] values = database.SetMembers(GetKey());
+            values.Where(x => x.HasValue)
+                  .Select(x => (string)x)
+                  .ToList()
+                  .ForEach(Console.WriteLine);
+        }
+
+        private static RedisKey GetKey()
+        {
+            return "TestKey";
+        }
+
+        private static void Main()
+        {
+            _redis = ConnectionMultiplexer.Connect("localhost");
+            bool isRun = true;
+            while (isRun)
+            {
+                Console.WriteLine("Press <Esc> to Exit or <Enter> to Get data");
+
+                ConsoleKeyInfo key = Console.ReadKey();
+                switch (key.Key)
+                {
+                    case ConsoleKey.Escape:
+                        isRun = false;
+                        break;
+                    case ConsoleKey.Enter:
+                        GetData();
+                        break;
+                }
+            }
+
+            _redis.Dispose();
+            Console.WriteLine("Bye");
+        }
+    }
+}
